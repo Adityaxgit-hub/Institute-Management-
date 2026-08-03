88802023fd0b1c88bf3014df8bfb242606c473ca
Abbhiinay <abbhiinaygudimalla@gmail.com>
2026-08-03 18:54:27 +0530

    added health before csrf

--- server.js
+++ server.js
@@ -106,7 +106,9 @@ app.use((req, res, next) => {
   next();
 });
 
-
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 app.use(csrfProtection);
 
 // Prevent caching of HTML files to avoid back-button exposure after logout
